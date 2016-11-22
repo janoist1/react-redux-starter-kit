d8a6b8b10a53840a5f5339d662255a865e3f0dd6
Tycho Bokdam <t.bokdam@seronaris.nl>
2016-11-22 10:24:03 +0100

    Fixes 'Unexpected Keys' for async

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -4,7 +4,8 @@ import { routerReducer as router } from 'react-router-redux'
 // Fix: "React-Redux: Combining reducers: Unexpected Keys"
 // http://stackoverflow.com/a/33678198/789076
 const initialReducers = {
-  counter: (state = 0) => state // default value should be imported from the module/reducer
+  counter: (state = 0) => state, // default value should be imported from the module/reducer
+  async: (state = {}) => state // default value should be imported from the module/reducer
 }
 
 export const makeRootReducer = (asyncReducers) => {
